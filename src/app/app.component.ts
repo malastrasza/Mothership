0d6a461d9dbacd9d197f261d72986b061d018b4e
malastrasza <malastrasza@gmail.com>
2023-09-25 16:47:50 +0200

    Setting application basic structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +6,4 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  sliderFormControl = new FormControl<number>(1);
-
-  sliderFormControl2 = new FormControl<number>(20);
-  sliderFormControl3 = new FormControl<number[]>([25,50]);
-
-
-  rangeChanged(event: any) {
-    // console.log('event: ', event)
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InfoComponent } from './info/info.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PriorityErrorPipe } from './pipes/priority-error.pipe';
-import { CapitalLetterDirective } from './directives/capitalLetter.directive';
-import { AuiSliderComponent } from './components/slider/slider.component';
+import { ReactiveFormComponent } from './courses/angular-university/forms/components/reactive-form/reactive-form.component';
+import { InfoComponent } from './courses/angular-university/forms/components/template-driven-form/info.component';
+import { CapitalLetterDirective } from './courses/angular-university/forms/directives/capitalLetter.directive';
+import { PriorityErrorPipe } from './courses/angular-university/forms/pipes/priority-error.pipe';
+import { AuiSliderComponent } from './my/custom-components/slider/slider.component';
+import { NavComponent } from './nav/nav.component';
 // import { AuiSliderModule } from './components/slider/slider.module';
 
 @NgModule({
-  declarations: [AppComponent, InfoComponent, PriorityErrorPipe, CapitalLetterDirective, AuiSliderComponent],
+  declarations: [
+    AppComponent,
+    InfoComponent,
+    PriorityErrorPipe,
+    CapitalLetterDirective,
+    AuiSliderComponent,
+    ReactiveFormComponent,
+    NavComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+    // Angular Material Imports
     FormsModule,
-    ReactiveFormsModule
-    
+    ReactiveFormsModule,
+    MatStepperModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
